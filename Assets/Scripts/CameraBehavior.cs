9ca6bc91ad84f1e3bff9390137eafe335ca78009
grace-siegwald <siegwald@colum.edu>
2025-10-06 23:29:25 -0500

    Chapter 6 & 7 Commit

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Vector3 CamOffset = new Vector3(0f, 1.2f, -2.6f);
+    private Transform _target;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = _target.TransformPoint(CamOffset);
+        this.transform.LookAt(_target);
+    }
+}

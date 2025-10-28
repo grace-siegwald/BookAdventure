2c8a1032a00382de7a4b55b003adb7de930e1bc6
grace-siegwald <siegwald@colum.edu>
2025-10-27 19:41:38 -0500

    Partway through chapter 8 commit

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -5,8 +5,17 @@ public class PlayerBehavior : MonoBehaviour
 {
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
+
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
+
     private float _vInput;
     private float _hInput;
+
+    public float DistanceToGround = 0.1f;
+    public LayerMask GroundLayer;
+    private CapsuleCollider _col;
+
 
     private Rigidbody _rb;
 
@@ -14,11 +23,14 @@ public class PlayerBehavior : MonoBehaviour
     void Start()
     {
         _rb = this.GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _isJumping |= Input.GetKeyDown(KeyCode.Space);
+        
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
 
@@ -35,5 +47,18 @@ public class PlayerBehavior : MonoBehaviour
 
         _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.deltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if (IsGrounded() && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        _isJumping = false;
+    }
+    
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+        return grounded;
     }
 }
